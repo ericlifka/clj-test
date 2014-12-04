6f6092c7ba223e13e8f677b467919987171d9729
Eric Lifka <eric.lifka@gmail.com>
2014-12-04 00:05:58 -0500

    compiling clojurescript file

--- project.clj
+++ project.clj
@@ -3,8 +3,15 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :plugins [[lein-cljsbuild "1.0.3"]]
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [clj-http "1.0.1"]]
+                 [clj-http "1.0.1"]
+                 [org.clojure/clojurescript "0.0-2371"]]
+  :cljsbuild {
+    :builds [{:source-paths ["src-cljs"]
+              :compiler {:output-to "resources/public/js/main.js"
+                         :optimizations :whitespace
+                         :pretty-print true}}]}
   :main ^:skip-aot clj-test.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
