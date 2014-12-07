851e6e9472369cb36e7c53d0977b3c8dafa3e5a2
Eric Lifka <eric.lifka@gmail.com>
2014-12-07 13:37:44 -0500

    clear function to reset the canvas for each frame

--- src-cljs/hello.cljs
+++ src-cljs/hello.cljs
@@ -10,12 +10,19 @@
     (.appendChild js/document.body canvas)
     (.getContext canvas "2d")))
 
-(defn render [context]
+(defn clear-for-frame [graphics-context]
+  (let [canvas (aget graphics-context "canvas")
+        width (aget canvas "width")
+        height (aget canvas "height")]
+    (.clearRect graphics-context 0 0 width height)))
+
+(defn render [graphics-context]
+  (clear-for-frame graphics-context)
   (.log js/console "frame"))
 
 (defn ^:export main []
-  (let [context (create-canvas)]
+  (let [graphics-context (create-canvas)]
     (letfn [(anim-frame []
-              (render context)
-              (.requestAnimationFrame js/window anim-frame))]
+              (render graphics-context)
+              (.requestAnimationFrame js/window anim-frame))] m .
       (anim-frame))))
